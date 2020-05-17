898f823f01ca6bd64f97d31ee20312d845054224
Chris Kramer <ChrisKramer15@outlook.com>
2020-05-17 17:06:30 -0500

    Manager Class completed and exported

--- files/lib/Manager.js
+++ files/lib/Manager.js
@@ -2,7 +2,16 @@
 const Employee = require('./Employee');
 
 class Manager extends Employee {
-    constructor (id, email, officeNum) {
-        super
+    constructor (name, id, email, officeNum) {
+        super(name, id, email);
+        this.officeNum = officeNum;
+    }
+    getRole() {
+        return "Manager";
+    }
+    getOfficeNum() {
+        return this.officeNum;
     }
 }
+
+module.exports= Manager;
